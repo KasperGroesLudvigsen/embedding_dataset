791aa8ab06687ad85798ed4b54487c1ca7c72baa
Kasper Groes Ludvigsen <groes.ludvigsen@gmail.com>
2025-01-23 21:33:07 +0100

    update shell script

--- start_script.sh
+++ start_script.sh
@@ -10,16 +10,20 @@ fi
 PYTHON_FILE_1=$1
 PYTHON_FILE_2=$2
 
+# Extract the base names of the Python files (without the directory and extension)
+LOG_FILE_1=$(basename "$PYTHON_FILE_1" .py).log
+LOG_FILE_2=$(basename "$PYTHON_FILE_2" .py).log
+
 # Run the first Python file with nohup
 echo "Running $PYTHON_FILE_1 in the background..."
-nohup python3 $PYTHON_FILE_1 > script1.log 2>&1 &
+nohup python3 $PYTHON_FILE_1 > "$LOG_FILE_1" 2>&1 &
 
 # Save the PID of the first script
 PID1=$!
 
 # Run the second Python file with nohup
 echo "Running $PYTHON_FILE_2 in the background..."
-nohup python3 $PYTHON_FILE_2 > script2.log 2>&1 &
+nohup python3 $PYTHON_FILE_2 > "$LOG_FILE_2" 2>&1 &
 
 # Save the PID of the second script
 PID2=$!
@@ -28,4 +32,4 @@ echo "Scripts are running in the background."
 echo "PID of $PYTHON_FILE_1: $PID1"
 echo "PID of $PYTHON_FILE_2: $PID2"
 
-echo "Logs are being written to script1.log and script2.log."
+echo "Logs are being written to $LOG_FILE_1 and $LOG_FILE_2."
