9e484350907d07c8e026dda3b17c7adb0dad01b9
Kasper Groes Ludvigsen <groes.ludvigsen@gmail.com>
2025-01-24 19:44:21 +0100

    set samples to 50k for swedish and norwegian

--- variables.py
+++ variables.py
@@ -2,7 +2,7 @@ import os
 
 model_id="google/gemma-2-27b-it"
 hf_user="ThatsGroes" # whose user to save data to
-total_desired_samples=100000 # per data type, i.e. total_desired_samples for classification, total_desired_samples for retrieval, total_desired_samples for unit triple etc.
+total_desired_samples=50000 # per data type, i.e. total_desired_samples for classification, total_desired_samples for retrieval, total_desired_samples for unit triple etc.
 total_desired_tasks=total_desired_samples//2
 temperature = 1.0
 top_p = 1.0
